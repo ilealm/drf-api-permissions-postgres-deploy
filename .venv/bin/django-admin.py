eec60e78bed30220c3d2ed36607f212694467109
Iris Leal <iris_leal_m@hotmail.com>
2020-06-25 12:57:50 -0700

    Remove git ignored files

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ileal/codefellows/401/drf-api-permissions-postgres-deploy/.venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
